e46e016a7c3854e39e189fc793d41105d6e10488
PeterDavid3800 <davidpeter487@gmail.com>
2023-11-25 22:45:00 +0300

    commit

--- BIProject.R
+++ BIProject.R
@@ -35,6 +35,8 @@ if (!requireNamespace("corrplot", quietly = TRUE)) {
 }
 library(corrplot)
 library(readr)
+library(boot)
+
 Argos_UK <- read_csv("data/Argos_UK.csv")
 
 # View the data
@@ -48,7 +50,7 @@ sapply(Argos_UK, class)
 Argos_UK <- as.data.frame(lapply(Argos_UK, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))
 
 # Frequency and percentage for selected columns
-columns_to_analyze <- c(2,4,7)
+columns_to_analyze <- c(2, 4, 7)
 for (col in columns_to_analyze) {
   Argos_UK_freq <- Argos_UK[[col]]
   freq_table <- table(Argos_UK_freq)
@@ -75,7 +77,6 @@ print(Argos_UK_ProductType_mode)
 summary(Argos_UK)
 any(is.na(Argos_UK))
 Argos_UK <- as.data.frame(lapply(Argos_UK, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))
-
 
 # Descriptive statistics for "Product Price"
 sapply(Argos_UK[c(7)], sd)
@@ -141,4 +142,71 @@ Argos_UK[, numeric_columns] <- lapply(Argos_UK[, numeric_columns], function(x) {
 categorical_columns <- sapply(Argos_UK, function(x) is.factor(x) | is.character(x))
 Argos_UK <- model.matrix(~., Argos_UK[, c(7, categorical_columns)], drop = TRUE)
 
+# Load libraries
+library(boot)
+library(caret)
 
+# Load data
+Argos_UK <- read_csv("data/Argos_UK.csv")
+
+# Display the first few rows of the data
+head(Argos_UK)
+
+# Replace missing values with column means
+Argos_UK <- as.data.frame(lapply(Argos_UK, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))
+
+# Check for missing values
+if (any(is.na(Argos_UK))) {
+  # Handle missing values (e.g., imputation)
+  Argos_UK <- na.omit(Argos_UK)
+}
+
+# Check for multicollinearity
+cor_matrix <- cor(Argos_UK[, sapply(Argos_UK, is.numeric)])
+highly_correlated <- findCorrelation(cor_matrix, cutoff = 0.8)
+if (length(highly_correlated) > 0) {
+  # Address multicollinearity (e.g., remove highly correlated variables)
+  Argos_UK <- Argos_UK[, -highly_correlated]
+}
+
+# Data Splitting
+set.seed(123)  # Set seed for reproducibility
+train_indices <- createDataPartition(Argos_UK$Product.Price, p = 0.7, list = FALSE)
+train_data <- Argos_UK[train_indices, ]
+test_data <- Argos_UK[-train_indices, ]
+
+# Retrain the model
+boot_model <- lm(Product.Price ~ ., data = train_data)
+
+# Bootstrapping with check for model convergence
+boot_results <- boot(data = train_data, statistic = function(data, indices) {
+  fit <- try(lm(Product.Price ~ ., data = data[indices, ]), silent = TRUE)
+  
+  if (inherits(fit, "try-error") || length(coef(fit)) != length(coef(boot_model))) {
+    return(rep(NA, length(coef(boot_model))))
+  }
+  
+  return(coef(fit))
+}, R = 1000)
+
+# Cross-validation (example using linear regression)/error starts here
+cv_model <- try(
+  train(Product.Price ~ ., data = train_data, method = "lm", trControl = trainControl(method = "cv")),
+  silent = TRUE
+)
+
+# Check for errors in cross-validation
+if (inherits(cv_model, "try-error")) {
+  cat("Error occurred during cross-validation training.\n")
+  print(cv_model)
+} else {
+  # Print summary
+  summary(cv_model)
+}
+
+# Model training (linear regression)
+final_model <- lm(Product.Price ~ ., data = train_data)
+
+# Model performance comparison using resamples
+resamples_list <- resamples(list(Linear_Regression = final_model, CV_Model = cv_model))
+summary(resamples_list)
